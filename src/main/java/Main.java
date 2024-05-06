cf6edd9b17f253bafd89b6afc33f4ac2159762f2
otavioarcanjo <33862543-otavioarcanjo@users.noreply.replit.com>
2024-05-06 15:18:11 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("OLÁ MUNDO!");
+    System.out.print("Tavix444");
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
